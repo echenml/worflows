5d8927357268ec025311675166ef355994c9b2ef
Eric Chen <echenml@gmail.com>
2017-02-15 00:13:26 -0500

    Concatenated Javascript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp=require('gulp');
 var gutil=require('gulp-util');
 var coffee=require('gulp-coffee');
+var concat=require('gulp-concat');
+
 var coffeeSources=['components/coffee/tagline.coffee'];
+var jsSources=[
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+]
 
 gulp.task('log',function(){
   gutil.log('Workflows are awesome');
@@ -17,4 +25,10 @@ gulp.task('coffee',function(){
   	.on('error',gutil.log))
   .pipe(gulp.dest('components/scripts'))
   	
+});
+
+gulp.task('js',function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
